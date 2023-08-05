6f1fc897242e3afa69d4786811e95afc59d433bc
danielmajchrak <daniel.majchrak@black-hole.sk>
2023-08-05 18:23:16 +0200

    Added basic User module and service

---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.prisma.user.findFirst({ where: { username: username } });
+  }
+}
