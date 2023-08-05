7a0c603e1d1c5c0546b70b2d0c68754a5511dea9
danielmajchrak <daniel.majchrak@black-hole.sk>
2023-08-05 18:59:02 +0200

    Changed where clause for finding user to email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,7 @@ import { User } from '@prisma/client';
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.prisma.user.findFirst({ where: { username: username } });
+  async findOne(email: string): Promise<User | undefined> {
+    return this.prisma.user.findFirst({ where: { email: email } });
   }
 }
